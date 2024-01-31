53be1e3dc82a13f2ce5c0b93927135f983c4e951
Hank Caswell <david.h.caswell@proton.me>
2024-01-31 16:10:43 -0500

    First attempt at an answer

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,9 +1,23 @@
 function toRomanLazy(num) {
-  return "";
+  let output = ""
+  let romanNumeraltoArabic = {"M": 1000, "D": 500, "C": 100, "L": 50, "X": 10, "V": 5, "I": 1}
+  let romanNumberPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+  for(let i = 0; i<romanNumberPriorityOrder.length; i++){
+    while(num >= romanNumeraltoArabic[romanNumberPriorityOrder[i]])  {
+      output += romanNumberPriorityOrder[i]
+      num -= romanNumeraltoArabic[romanNumberPriorityOrder[i]]
+    }
+
+  }
+  return output;
 }
+
+
+
 
 function toRoman(num) {
   return "";
 }
 
 module.exports = { toRoman, toRomanLazy };
+
